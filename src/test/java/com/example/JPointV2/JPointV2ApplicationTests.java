7ce4c71476fb8bfd22727c8bb6d88443dd2cca7e
WoldemarK <kovtynov.vladimir@gmail.com>
2023-02-28 09:52:21 +0300

    create UserServiceTest

---
+++ src/test/java/com/example/JPointV2/JPointV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JPointV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JPointV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
